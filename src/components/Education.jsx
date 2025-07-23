2120b44d174646b99580ef6afe0bad5170d0a1e1
IteratorInnovator <harryngkokjing@gmail.com>
2025-07-23 16:10:35 +0530

    Redesign button UI

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SectionHeader from "./SectionHeader";
+import SectionHeader from "./ui/section-header";
 
 function Education() {
     return (
@@ -9,7 +9,13 @@ function Education() {
         >
             <SectionHeader title="Education" />
             {/* Timeline + cards wrapper */}
-            
+            <div class="relative block max-w-6xl mx-auto">
+                <div class="flex">
+                    <div class="w-32 flex-shrink-0 relative">
+                        <div class="absolute w-1 left-6 top-0 bottom-0 bg-gradient-to-b from-blue-500 via-cyan-500 to-purple-500 rounded-full"></div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
