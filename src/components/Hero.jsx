0af0c8f88929a8ee35cf8aa5ac0ae4616f92d489
IteratorInnovator <harryngkokjing@gmail.com>
2025-11-15 02:32:10 +0800

    404 Not Found Page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collisi
 import { Button } from "./ui/moving-border";
 import { Link } from "react-router-dom";
 import Resume from "../assets/Ng Kok Jing Resume.pdf";
+import { section } from "motion/react-client";
 
 const phrases = [
     "Software Engineer.",
@@ -51,45 +52,47 @@ const Hero = () => {
         }
     }, [charIndex, isDeleting, phraseIndex]);
     return (
-        <BackgroundBeamsWithCollision>
-            <section className="mt-16">
-                <h1 className="text-6xl font-jetbrains text-center font-medium text-primary">
-                    Hi, I'm <span className="text-accent">Harry!</span>
-                </h1>
-                <h2 className="text-primary text-6xl text-left mt-6 font-jetbrains font-medium">
-                    I'm a
-                    <span
-                        id="typewriter"
-                        className="ml-8 text-6xl font-jetbrains font-bold text-accent"
-                    >
-                        {text}
-                    </span>
-                </h2>
-                <p className="font-inter text-xl text-secondary text-center max-w-3xl mx-auto my-12">
-                    Passionate about creating cutting-edge solutions that bridge
-                    the gap between innovative technology and real-world
-                    applications. Specializing in backend development,
-                    automation, and devops.
-                </p>
-                <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
-                    <Link
-                        to="/projects"
-                        className="w-full sm:w-auto bg-accent text-white px-8 py-3 rounded-full font-jetbrains font-semibold text-lg text-center transition-colors duration-200 hover:bg-accent-hover"
-                    >
-                        View Projects
-                    </Link>
-                    <Button
-                        as="a"
-                        href={Resume}
-                        download
-                        containerClassName="w-full sm:w-auto"
-                        className="w-full sm:w-auto px-8 py-3 font-jetbrains font-semibold border border-slate-200 text-lg text-center text-accent bg-neutral-100 transition-all duration-300 hover:border-neutral-300"
-                    >
-                        Download Resume
-                    </Button>
+        <section id="home">
+            <BackgroundBeamsWithCollision>
+                <div className="mt-16">
+                    <h1 className="text-6xl font-jetbrains text-center font-medium text-primary">
+                        Hi, I'm <span className="text-accent">Harry!</span>
+                    </h1>
+                    <h2 className="text-primary text-6xl text-left mt-6 font-jetbrains font-medium">
+                        I'm a
+                        <span
+                            id="typewriter"
+                            className="ml-8 text-6xl font-jetbrains font-bold text-accent"
+                        >
+                            {text}
+                        </span>
+                    </h2>
+                    <p className="font-inter text-xl text-secondary text-center max-w-3xl mx-auto my-12">
+                        Passionate about creating cutting-edge solutions that
+                        bridge the gap between innovative technology and
+                        real-world applications. Specializing in backend
+                        development, automation, and devops.
+                    </p>
+                    <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+                        <Link
+                            to="/projects"
+                            className="w-full sm:w-auto bg-accent text-white px-8 py-3 rounded-full font-jetbrains font-semibold text-lg text-center transition-colors duration-200 hover:bg-accent-hover"
+                        >
+                            View Projects
+                        </Link>
+                        <Button
+                            as="a"
+                            href={Resume}
+                            download
+                            containerClassName="w-full sm:w-auto"
+                            className="w-full sm:w-auto px-8 py-3 font-jetbrains font-semibold border border-slate-200 text-lg text-center text-accent bg-neutral-100 transition-all duration-300 hover:border-neutral-300"
+                        >
+                            Download Resume
+                        </Button>
+                    </div>
                 </div>
-            </section>
-        </BackgroundBeamsWithCollision>
+            </BackgroundBeamsWithCollision>
+        </section>
     );
 };
 
